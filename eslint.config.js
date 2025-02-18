73b30fba0e3cd336c3b0e7deeb028091f6d28249
Samapa1 <saija.m.pajari@gmail.com>
2025-02-18 14:28:59 +0200

    10.2

---
+++ eslint.config.js
@@ -0,0 +1,54 @@
+import react from "eslint-plugin-react";
+import reactNative from 'eslint-plugin-react-native';
+import babelParser from "@babel/eslint-parser";
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+      rules: {
+        "react/prop-types": "off",
+        "react/react-in-jsx-scope": "off"
+      },
+      plugins: {
+        react, 
+        reactNative
+        
+      },
+      settings: {
+        "react": {
+          "version": "detect"
+        }
+      },
+      languageOptions: {
+        parser: babelParser,
+        globals: {
+          ...globals.reactNative
+        },
+        parserOptions: {
+          ecmaVersion: "latest",
+          ecmaFeatures: { jsx: true }
+        },
+      },
+  },
+
+];
+
+// {
+//     "plugins": ["react", "react-native"],
+//     "settings": {
+//       "react": {
+//         "version": "detect"
+//       }
+//     },
+//     "extends": ["eslint:recommended", "plugin:react/recommended"],
+//     "parser": "@babel/eslint-parser",
+//     "env": {
+//       "react-native/react-native": true
+//     },
+//     "rules": {
+//       "react/prop-types": "off",
+//       "react/react-in-jsx-scope": "off"
+//     }
+//   }
